7dab4aa2d9e0dfc0b2be225be72009f3ec2bc9dd
downpour07 <choiheeyoung10@gmail.com>
2025-03-07 09:47:50 +0900

    setvar update

---
+++ lotura/lib/setvar/setvar.h
@@ -0,0 +1,9 @@
+#ifndef _SETVAR_H
+#define _SETVAR_H
+
+#include "variable.h"
+
+int CH1_SETVAR(String SerialData, int dex1, int dexc, int end);
+int CH2_SETVAR(String SerialData, int dex1, int dexc, int end);
+
+#endif
